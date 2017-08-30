e4f57f325199cdbcb8845161cf79dfcbd4de3266
tubaplaya21 <benjamin21miller@gmail.com>
2017-08-30 10:36:02 -0500

    Board is actually console based.

--- checkers.js
+++ checkers.js
@@ -195,42 +195,13 @@ function nexTurn(){
 }
 
 function printBoard(){
-  var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext('2d');
-
-  ctx.fillStyle = 'red';
-  ctx.fillRect(10, 30, 20, 20);
-  ctx.fillRect(10, 70, 20, 20);
-  ctx.fillRect(10, 110, 20, 20);
-  ctx.fillRect(10, 150, 20, 20);
-  ctx.fillRect(30, 10, 20, 20);
-  ctx.fillRect(30, 50, 20, 20);
-  ctx.fillRect(30, 90, 20, 20);
-  ctx.fillRect(30, 130, 20, 20);
-  ctx.fillRect(50, 30, 20, 20);
-  ctx.fillRect(50, 70, 20, 20);
-  ctx.fillRect(50, 110, 20, 20);
-  ctx.fillRect(50, 150, 20, 20);
-  ctx.fillRect(70, 10, 20, 20);
-  ctx.fillRect(70, 50, 20, 20);
-  ctx.fillRect(70, 90, 20, 20);
-  ctx.fillRect(70, 130, 20, 20);
-  ctx.fillRect(90, 30, 20, 20);
-  ctx.fillRect(90, 70, 20, 20);
-  ctx.fillRect(90, 110, 20, 20);
-  ctx.fillRect(90, 150, 20, 20);
-  ctx.fillRect(110, 10, 20, 20);
-  ctx.fillRect(110, 50, 20, 20);
-  ctx.fillRect(110, 90, 20, 20);
-  ctx.fillRect(110, 130, 20, 20);
-  ctx.fillRect(130, 30, 20, 20);
-  ctx.fillRect(130, 70, 20, 20);
-  ctx.fillRect(130, 110, 20, 20);
-  ctx.fillRect(130, 150, 20, 20);
-  ctx.fillRect(150, 10, 20, 20);
-  ctx.fillRect(150, 50, 20, 20);
-  ctx.fillRect(150, 90, 20, 20);
-  ctx.fillRect(150, 130, 20, 20);
+  state.board.forEach(function(row){
+    var ascii = row.map(function(square){
+      if(square) return '_';
+      else return square;
+    });
+    console.log(ascii);
+  });
 }
 
 function main(){
